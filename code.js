89fff246979903c156a0851a7356486024564756
Mon1Amour4 <mon1amour4@gmail.com>
2023-07-27 18:21:53 +0800

    Добавил инпут
    Добавил инпут и кое что по мелочи

--- code.js
+++ code.js
@@ -6,6 +6,48 @@ function clickFun() {
   console.log("Click");
 }
 
+function createDiv(button) {
+  button.addEventListener("click", () => {
+    const createDiv = document.createElement("div");
+    createDiv.className = "Column";
+    createDiv.id = "myColumn";
+    // createDiv.textContent = "Input your text here";
+    const findcontainerRow = document.getElementById("containerRow");
+    findcontainerRow.insertAdjacentElement("afterend", createDiv);
+  });
+}
+
+// )
+
+function createColumnItem(button) {
+  button.addEventListener("click", () => {
+    const findMyColumn = document.getElementById("myColumn");
+    for (let index = 0; index < 4; index++) {
+      const createcolumnItem = document.createElement("input");
+      createcolumnItem.className = "ColumnItem";
+      createcolumnItem.id = "ColumnItemMyId" + '_' + (index + 1);
+      createcolumnItem.style.width = "25%";
+      // createcolumnItem.innerText = "ColumnItem";
+      findMyColumn.appendChild(createcolumnItem);
+    }
+  });
+}
+function createP(button) {
+  button.addEventListener("click", () => {
+    for (let index = 0; index < 4; index++) {
+      const findMyColumnItem = document.getElementById("ColumnItemMyId" + '_' + (index+1));
+      const createP = document.createElement("p");
+      findMyColumnItem.appendChild(createP);
+      createP.innerText = "TextMyPppppppppppppppppppppppppp";
+      const textContent = findMyColumnItem.textContent;
+      const textLength = textContent.length;
+      const height = textLength*100;
+      findMyColumnItem.height = height;
+    }
+
+  });
+}
+
 window.onload = function () {
   const myButton = document.querySelector(".btn");
 
@@ -14,13 +56,7 @@ window.onload = function () {
   myButton.addEventListener("click", clickFun);
 
   // Добавление элемента
-
-
-  myButton.addEventListener("click", () => {
-    const createContainer = document.createElement('div');
-    createContainer.className = "Column";
-    createContainer.innerText = "asdasd";
-    console.log(createContainer);
-    myButton.insertAdjacentElement("afterend", createContainer);
-  });
+  createDiv(myButton);
+  createColumnItem(myButton);
+  createP(myButton);
 };
