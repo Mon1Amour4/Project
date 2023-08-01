f76309ed10d5b5eb9e8ac2e430dd966340912fb1
Mon1Amour4 <mon1amour4@gmail.com>
2023-08-01 17:47:53 +0800

    1

--- code.js
+++ code.js
@@ -11,48 +11,65 @@ function createColumn(button) {
   button.addEventListener("click", () => {
     const createDiv = document.createElement("div");
     createDiv.className = "Column";
-    createDiv.id = "myColumn";
-    // createDiv.textContent = "Input your text here";
-    const findcontainerRow = document.getElementById("containerRow");
-    findcontainerRow.insertAdjacentElement("afterend", createDiv);
+    createDiv.id = "myColumn" + "_" + counter;
+    if (counter == 0) {
+      const findcontainerRow = document.getElementById("containerRow");
+      findcontainerRow.insertAdjacentElement("afterend", createDiv);
+    } else {
+      const findMyColumnMinusCounter = document.getElementById(
+        "myColumn" + "_" + (counter - 1)
+      );
+      findMyColumnMinusCounter.insertAdjacentElement("afterend", createDiv);
+    }
     counter += 1;
   });
 }
 
-// )
-
 function createColumnItem(button) {
   button.addEventListener("click", () => {
-    const findMyColumn = document.getElementById("myColumn");
+    const findMyColumn = document.getElementById(
+      "myColumn" + "_" + (counter - 1)
+    );
     for (let index = 0; index < 4; index++) {
-      const createcolumnItem = document.createElement("input");
-      createcolumnItem.className = "ColumnItem";
-      createcolumnItem.id = "ColumnItemMyId" + "_" + (index + 1);
-      createcolumnItem.style.width = "25%";
-      createcolumnItem.style.backgroundColor = "red";
-      // createcolumnItem.innerText = "ColumnItem";
-      findMyColumn.appendChild(createcolumnItem);
+      if (index === 0) {
+        const createCheckboxWrapper = document.createElement("div");
+        const createCheckbox = document.createElement("input");
+        createCheckboxWrapper.id =
+          "createCheckboxWrapper" + "_" + (counter - 1);
+        createCheckboxWrapper.style.display = "flex";
+        createCheckboxWrapper.border = "1px dashed blue";
+        createCheckboxWrapper.style.width = "3vw";
+        createCheckbox.type = "checkbox";
+        createCheckbox.name = "myCheckbox";
+        createCheckbox.style.margin = "0 auto";
+        createCheckbox.style.width = "1.5vw";
+        findMyColumn.appendChild(createCheckboxWrapper);
+        const findCheckboxWrapper = document.getElementById(
+          "createCheckboxWrapper" + "_" + (counter - 1)
+        );
+        findCheckboxWrapper.appendChild(createCheckbox);
+      } else {
+        const createcolumnItem = document.createElement("textarea");
+        createcolumnItem.className = "ColumnItem";
+        createcolumnItem.id = "ColumnItemMyId" + "_" + (index + 1);
+        createcolumnItem.style.backgroundColor = "red";
+        findMyColumn.appendChild(createcolumnItem);
+      }
     }
   });
 }
-// function createP(button) {
-//   button.addEventListener("click", () => {
-//     for (let index = 0; index < 4; index++) {
-//       const findMyColumnItem = document.getElementById("ColumnItemMyId" + '_' + (index+1));
-//       const createP = document.createElement("p");
-//       findMyColumnItem.appendChild(createP);
-//       // createP.innerText = "TextMyPppppppppppppppppppppppppp";
-//       // const textContent = findMyColumnItem.textContent;
-//       // const textLength = textContent.length;
-//       // const height = textLength*100;
-//       // findMyColumnItem.height = height;
-//     }
 
-//   });
+function saveData() {
+  for (let index = 0; index < counter; index++) {
+    if (counter != 0) {
+      const getRow = document.getElementById("myColumn" + "_" + (counter - 1));
+      getRow.ATTRIBUTE_NODE;
+    }
+  }
+}
 
 window.onload = function () {
   const myButton = document.querySelector(".btn");
-
   console.log(myButton);
 
   myButton.addEventListener("click", clickFun);
@@ -60,5 +77,4 @@ window.onload = function () {
   // Добавление элемента
   createColumn(myButton);
   createColumnItem(myButton);
-  createP(myButton);
 };
